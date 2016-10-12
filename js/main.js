bd8461226760655c7d8934a6c753edeacbb0bdc3
shan ho <hoshan@gmail.com>
2016-10-11 22:05:51 -0700

    5th repo

--- js/main.js
+++ js/main.js
@@ -62,6 +62,9 @@ $("#scissor").on("click", function() {
 
 
 
+
+
+
 //computer turn
 function turn () {
   //call AI in to a var
@@ -107,7 +110,30 @@ function AI() {
   return cPu;
 }
 
+//make it rain
 
+// number of drops created.
+var nbDrop = 858;
 
+// function to generate a random number range.
+function randRange( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createRain() {
+
+  for( i=1;i<nbDrop;i++) {
+  var dropLeft = randRange(0,1600);
+  var dropTop = randRange(-1000,1400);
+
+  $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+  $('#drop'+i).css('left',dropLeft);
+  $('#drop'+i).css('top',dropTop);
+  }
+
+}
+// Make it rain
+createRain();
 
 
